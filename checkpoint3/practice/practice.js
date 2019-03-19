0aabbe54107f6e04f0f5e28820405dbcaae199c8
asa1411 <jungil_cho@yahoo.com>
2019-03-18 22:27:26 -0500

    checkpoint button functions

--- checkpoint3/practice/practice.js
+++
@@ -1,6 +0,0 @@
-let a = 2;
-function add() {
-  a++;
-  document.getElementById("add").innerHTML = a;
-  return true;
-}
--- script.js
+++ script.js
@@ -2,6 +2,15 @@ var score = 0;
 function btnClicked() {
   score++;
   document.getElementById("score").innerHTML = score;
-  console.log(score);
+
   return true;
 }
+function buy1() {
+  score = score - 5;
+  document.getElementById("score").innerHTML = score;
+  return true;
+}
+
+var myscore = setInterval((score += 2), 1000);
+
+console.log(myscore);
