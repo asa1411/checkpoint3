bbf417df3b1d625b0e22f13846aa9baffeb5a408
asa1411 <jungil_cho@yahoo.com>
2019-03-20 16:18:52 -0500

    selling popcorn makers

--- script.js
+++ script.js
@@ -17,25 +17,43 @@ function buySmallBaker() {
 
   document.getElementById("smallBakerNumber").innerHTML =
     "popcorn maker(s): " + baker1;
-  document.getElementById("score").innerHTML = score1;
+  // document.getElementById("score").innerHTML = score1;
 }
+onclick = "sellSmallBaker()";
+function sellSmallBaker() {
+  if (baker1 >= 1) {
+    baker1 = baker1 - 1;
+    score = score + 10;
+  }
+  document.getElementById("smallBakerNumber").innerHTML =
+    "popcorn maker(s): " + baker1;
+}
+
 onclick = "buyMediumBaker()";
 var baker2 = 0;
 function buyMediumBaker() {
   if (score >= 20) {
-    baker2 = baker2 + 2;
+    baker2 = baker2 + 1;
     score = score - 20;
   }
   document.getElementById("mediumBakerNumber").innerHTML =
-    "popcorn maker(s): " + baker2 / 2;
-  document.getElementById("score").innerHTML = score;
+    "popcorn maker(s): " + baker2;
 }
 // var nextBakerCost = Math.floor(10 * Math.pow(1.1, baker1));
-// document.getElementById("mediumBakerNumber").innerHTML = baker1;
+
+onclick = "sellMediumBaker()";
+function sellMediumBaker() {
+  if (baker2 >= 1) {
+    baker2 = baker2 - 1;
+    score = score + 20;
+  }
+  document.getElementById("mediumBakerNumber").innerHTML =
+    "popcorn maker(s): " + baker2;
+}
 
 window.setInterval(function() {
   btnClicked(baker1);
 }, 1000);
 window.setInterval(function() {
   btnClicked(baker2);
-}, 1000);
+}, 500);
